60b9949fd4f60550c52b455c3936489c992eb515
Sarah Reimann <reimannsarah45@gmail.com>
2023-04-19 14:38:24 -0700

    async chain to show AIC paintings
    Co-authored-by: Emma Gerigscott <elgerig@gmail.com>

--- src/index.js
+++ src/index.js
@@ -6,38 +6,21 @@ import ArtService from './artService.js';
 function getPaintings() {
   ArtService.getArtworkId()
     .then(response => {
-      console.log("1: " + response);
       return response;
     })
     .then(response2 => {
       const ids = getId(response2);
-      console.log("2: " + response2);
       return ArtService.getImageId(ids);
     })
     .then(response3 => {
-      console.log(response3)
-      const imageIdArray = [];
       response3.data.forEach(image => {
-        let promise = ArtService.getImage(image.image_id);
-        console.log(promise);
-        let jasonifiedImage = promise.json();
-        imageIdArray.push(jasonifiedImage);
+        ArtService.getImage(image.image_id)
+          .then(response => {
+            let image = document.createElement("img");
+            image.src = response.url;
+            document.querySelector('#output').append(image);
+          })
       })
-      // let imageId = response3.data.image_id;
-      // return ArtService.getImage(imageId);
-      console.log("3: ");
-      console.log(imageIdArray);
-      return imageIdArray;
-    })
-    .then(response4 => {
-      console.log(response4);
-      response4.forEach(promise => {
-        console.log(promise)
-      })
-      let image = document.createElement("img");
-      image.src = response4.url;
-      document.querySelector('#output').append(image);
-  
     });
 }
 
