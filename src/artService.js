1168b90e1e0f2b5eb30a30d0d00b97de180fbcf1
Sarah Reimann <reimannsarah45@gmail.com>
2023-04-20 10:50:59 -0700

    finish up error handling for individual images
     co-authored-by: Emma GerigScott <elgerig@gmail.com>

--- src/artService.js
+++ src/artService.js
@@ -1,6 +1,6 @@
 export default class ArtService {
   static async getArtworkId(userInput) {
-    return fetch(`https://api.artic.edu/api/v1/artworks/searchq=${userInput}&limit=20&fields=id`)
+    return fetch(`https://api.artic.edu/api/v1/artworks/search?q=${userInput}&limit=20&fields=id`)
       .then(response => {
         if (!response.ok) {
           const errorMessage = `${response.status} ${response.statusText}`;
@@ -15,13 +15,27 @@ export default class ArtService {
   static async getImageId(ids) {
     return fetch(`https://api.artic.edu/api/v1/artworks?ids=${ids}&fields=image_id`)
       .then(response => {
+        if (!response.ok) {
+          const errorMessage = `${response.status} ${response.statusText}`;
+          throw new Error(errorMessage);
+        }
         return response.json();
+      })
+      .catch(error => {
+        return error;
       });
   }
   static async getImage(imageId) {
     return fetch(`https://www.artic.edu/iiif/2/${imageId}/full/843,/0/default.jpg`)
       .then(response => {
+        if (!response.ok) {
+          const errorMessage = `${response.status} ${response.statusText}`;
+          throw new Error(errorMessage);
+        }
         return response;
+      })
+      .catch(error => {
+        return error;
       });
   }
 }
--- src/index.js
+++ src/index.js
@@ -18,12 +18,27 @@ function getPaintings(userInput) {
       return ArtService.getImageId(ids);
     })
     .then(response3 => {
+      if (response3 instanceof Error) {
+        const errorMessage = `Well gee I dunno what happened:
+        ${response3.message}`;
+        throw new Error(errorMessage);
+      }
       response3.data.forEach(image => {
+        console.log(response3);
         ArtService.getImage(image.image_id)
           .then(response => {
+            console.log(response);
+            if (response instanceof Error) {
+              const errorMessage = `Whoops! Couldn't find image: 
+              ${response.message}`;
+              throw new Error(errorMessage);
+            }
             let image = document.createElement("img");
             image.src = response.url;
             document.querySelector('#output').append(image);
+          })
+          .catch(error => {
+            imageError(error);
           });
       });
     })
@@ -32,8 +47,24 @@ function getPaintings(userInput) {
     });
 }
 
+function imageError(message) {
+  const outputDiv = document.querySelector('#output');
+  const image = document.createElement("img");
+  const p = document.createElement("p");
+
+  p.innerText = message;
+  image.src = "./assets/images/before.jpg";
+  p.append(image);
+  outputDiv.append(p);
+}
+
 function printError(message) {
-  document.getElementById("error").innerText = message;
+  const errorDiv = document.getElementById("error");
+  errorDiv.innerText = message;
+  
+  const img = document.createElement('img');
+  img.src = "./assets/images/during.jpg";
+  errorDiv.append(img);
 }
 
 function getId(response) {
