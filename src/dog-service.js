eea7d31eb1f490238cd7e14a80b3cc7cd9f070b0
Sarah Reimann <reimannsarah45@gmail.com>
2023-04-20 16:16:26 -0700

    add dog-service.js file to hold random dog API call
     co-authored-by: Emma GerigScott <elgerig@gmail.com>

---
+++ src/dog-service.js
@@ -0,0 +1,15 @@
+export default class DogService {
+  static async getDog() {
+    return fetch(`http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true`)
+      .then(response => {
+        if (!response.ok) {
+          const errorMessage = `${response.status} ${response.statusText}`;
+          throw new Error(errorMessage);
+        }
+        return response.json();
+      })
+      .catch(error => {
+        return error;
+      });
+  }
+}
